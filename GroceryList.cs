86bec267a815f057421170b85f02e5a8d505a26c
RosarioRu <mariarruval@gmail.com>
2022-06-25 15:21:20 -0700

    Comment out foreach section and refactor with for loop.

--- GroceryList.cs
+++ GroceryList.cs
@@ -1,24 +1,41 @@
 using System;
 
-class GroceryList
+class groceryList
 {
   static void Main()
   {
-    string[] myGroceryList = {"eggs", "milk", "bread", "bananas", "cereal", "rice", "yogurt"};
-    int[] groceryListPrices = {3, 6, 4, 2, 4, 4};
+    string[] myGroceryList = {"eggs", "milk", "bread", "bananas", "cereal", "rice"};
 
     Console.WriteLine("My grocery list:");
-
-    foreach (string item in myGroceryList)
+    for (int index = 0; index<myGroceryList.Length; index++)
     {
-      Console.WriteLine(item);
+      Console.WriteLine(myGroceryList[index]);
     }
-
-    int total = 0;
-    foreach (int price in groceryListPrices)
-    {
-      total = total + price;
-    }
-    Console.WriteLine("Your total for this shopping trip will be $" + total);
   }
 }
+
+
+
+
+//BELOW IS FOREACH LOOPS!!
+// class GroceryList
+// {
+//   static void Main()
+//   {
+//     string[] myGroceryList = {"eggs", "milk", "bread", "bananas", "cereal", "rice", "yogurt"};
+//     int[] groceryListPrices = {3, 6, 4, 2, 4, 4};
+
+//     Console.WriteLine("My grocery list:");
+//     foreach (string item in myGroceryList)
+//     {
+//       Console.WriteLine(item);
+//     }
+
+//     int total = 0;
+//     foreach (int price in groceryListPrices)
+//     {
+//       total = total + price;
+//     }
+//     Console.WriteLine("Your total for this shopping trip will be $" + total);
+//   }
+// }
